244eeb51a8e513fe74e4116be72fc6ff6aa80274
Veronica Garcia <veronica.garcia1187@gmail.com>
2020-08-24 10:15:07 -0400

    vide details y main Area

--- YouTubeProject (1)/YouTubeProject/src/main/java/youtube/components/YoutubeVideoDetailsComponent.java
+++ YouTubeProject (1)/YouTubeProject/src/main/java/youtube/components/YoutubeVideoDetailsComponent.java
@@ -1,4 +1,17 @@
 package youtube.components;
 
+import org.openqa.selenium.WebDriver;
+import youtube.pageobjects.leftpageobjects.LeftMenuPageObject;
+import youtube.pageobjects.videodetailspageobjects.VideoDetailsPageObjects;
+
 public class YoutubeVideoDetailsComponent {
+    private VideoDetailsPageObjects videoDetailsPageObjects;
+
+    public VideoDetailsPageObjects (WebDriver driver) {
+        this.videoDetailsPageObjects = new VideoDetailsPageObjects(driver, driver.getCurrentUrl());
+    }
+
+    public VideoDetailsPageObjects getVideoDetailsPageObjects() {
+        return videoDetailsPageObjects;
+    }
 }
---
+++ YouTubeProject (1)/YouTubeProject/src/main/java/youtube/usersteps/YoutubeMainAreaUserSteps.java
@@ -0,0 +1,28 @@
+package youtube.usersteps;
+
+import org.openqa.selenium.WebDriver;
+import youtube.pages.YoutubeHomePage;
+import youtube.pages.YoutubeMainAreaPage;
+
+public class YoutubeMainAreaUserSteps {
+
+    private YoutubeMainAreaPage youtubeMainAreaPage;
+    public YoutubeMainAreaUserSteps(WebDriver driver){
+        this.youtubeMainAreaPage = new YoutubeMainAreaPage(driver);
+    }
+
+    public void detailInformation(){
+
+    }
+
+    public void SelectVideoTHumbnail(){
+
+    }
+
+    public void NavigateDetailsPage(){
+
+    }
+
+
+
+}
